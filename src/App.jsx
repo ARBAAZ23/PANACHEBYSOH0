019a4844a88afa2762c11af4e63a931234fcf72b
Arbaaz <arbaaz231122@gmail.com>
2025-08-30 14:11:24 +0530

    change ofremove forgetPassword

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,6 @@ import Footer from "./components/Footer";
 import SearchBar from "./components/SearchBar";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css'
-import ForgotPassword from "./components/ForgotPassword";
-import backendUrl from  '../src/contexts/ShopContext'
 
 
 const App = () => {
@@ -34,7 +32,6 @@ const App = () => {
         <Route path="/login" element={<Login/>} />
         <Route path="/place-order" element={<PlaceOrder/>} />
         <Route path="/orders" element={<Orders/>} />
-        <Route path="/forgot-password" element={<ForgotPassword/>} />
       </Routes>
       <Footer/>
     </div>
